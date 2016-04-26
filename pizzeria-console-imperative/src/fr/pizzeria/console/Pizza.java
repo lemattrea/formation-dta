25ecabebb7deca6297b726e0ea8c5833f2d5cb6b
Albin Lemattre <lemattreal@gmail.com>
2016-04-26 16:52:36 +0200

    gestion pizza

---
+++ pizzeria-console-imperative/src/fr/pizzeria/console/Pizza.java
@@ -0,0 +1,17 @@
+package fr.pizzeria.console;
+
+public class Pizza {
+	public int id;
+	public String code;
+	public String nom;
+	public int prix;
+	
+	public Pizza(int identifiant, String code, String nom, int prix) {
+		this.id = identifiant;
+		this.code = code;
+		this.nom = nom;
+		this.prix = prix;
+	}
+	
+
+}
---
+++ pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -0,0 +1,96 @@
+package fr.pizzeria.console;
+
+import java.util.Scanner;
+
+public class PizzeriaAdminConsoleApp {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Object[][] pizza = getTableauPizza();
+		int choix = 0;
+		do {
+			System.out.println("*****	Pizzeria	Administration	***** \n"
+				+"1.	Lister les pizzas\n"	
+				+"2.	Ajouter une nouvelle pizza\n"
+				+"3.	Mettre à jour une pizza\n"
+				+"4.	Supprimer une pizza\n"
+				+"99.	Sortir");
+			
+			choix = sc.nextInt();
+			switch(choix){
+				case(1):
+					System.out.println("Liste des pizzas");
+					affichePizza(pizza);
+					break;
+				case(2):
+					System.out.println("Ajout d’une nouvelle pizza");
+					int caseVide = getNextInsert(pizza);
+					pizza = changePizza(pizza, caseVide, sc);
+					break;
+				case(3):
+					System.out.println("Mise à jour d’une pizza");
+					affichePizza(pizza);
+					System.out.println("Veuillez choisir la pizza à modifier.");
+					System.out.println("(99 pour abandonner)");
+					int numPizza = sc.nextInt();
+					if (numPizza != 99) {
+						pizza = changePizza(pizza, numPizza, sc);
+					}
+					
+					break;
+				case(4):
+					System.out.println("Suppression d’une pizza");
+					affichePizza(pizza);
+					System.out.println("Veuillez choisir la pizza à supprimer.");
+					System.out.println("(99 pour abandonner)");
+					int numberPizza = sc.nextInt();
+					if (numberPizza != 99) {
+						pizza[numberPizza] = new Object[3];
+					}
+					break;
+			}
+		}while(choix != 99);
+		
+		System.out.println("Aurevoir :(");
+		
+	}
+	
+	private static void affichePizza(Object[][] pizza) {
+		for(int i = 0; i < pizza.length; i++) {
+			if( pizza[i][0] != null) {
+				System.out.println(i+"->"+pizza[i][0]+"->"+pizza[i][1]+"->"+pizza[i][2]);
+			}
+		}
+	}
+	
+	private static Object[][] changePizza(Object[][] pizza, int numCase, Scanner sc){
+		System.out.println("Veuillez saisir le code");
+		pizza[numCase][0] = sc.next();
+		System.out.println("Veuillez saisir le nom (sans espace)");
+		pizza[numCase][1] = sc.next();
+		System.out.println("Veuillez saisir le prix");
+		pizza[numCase][2] = sc.nextDouble();
+		
+		return pizza;
+	}
+	
+	private static int getNextInsert(Object[][] pizza) {
+		int i = 0;
+		while(i < pizza.length && pizza[i][0] != null) {
+			i++;
+		}
+		return i;
+	}
+
+	private static Object[][] getTableauPizza() {
+		Object[][] pizza = new Object[100][3];
+		pizza[0] = new Object[]{"PEP","Pépéroni",12.50};
+		pizza[1] = new Object[]{"MAR","Margherita",12.50};
+		pizza[2] = new Object[]{"REI","La Reine",12.50};
+		pizza[3] = new Object[]{"FRO","La 4 fromages",12.50};
+		pizza[4] = new Object[]{"CAN","La cannibale",12.50};
+		pizza[5] = new Object[]{"SAV","La savoyarde",12.50};
+		pizza[6] = new Object[]{"ORI","L’orientale",12.50};
+		pizza[7] = new Object[]{"IND","L’indienne",12.50};
+		return pizza;
+	}
+}
