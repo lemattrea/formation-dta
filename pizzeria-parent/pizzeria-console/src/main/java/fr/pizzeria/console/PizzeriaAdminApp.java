8dfd0e976d840969105a99a7a463be044c65ac14
Albin Lemattre <lemattreal@gmail.com>
2016-05-11 09:35:17 +0200

    correction

--- pizzeria-parent/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAdminApp.java
+++ pizzeria-parent/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAdminApp.java
@@ -1,20 +1,39 @@
 package fr.pizzeria.console;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
-import fr.pizzeria.doa.*;
-import fr.pizzeria.ihm.menu.*;
-import fr.pizzeria.model.Pizza;
+import fr.pizzeria.doa.IPizzaDao;
+import fr.pizzeria.doa.PizzaDaoImplFile;
+import fr.pizzeria.doa.PizzaDaoImplMemory;
+import fr.pizzeria.ihm.menu.Menu;
 
 public class PizzeriaAdminApp {
 
 	public static void main(String[] args) {
-		//new Pizza().equals(new Pizza());
+
+		ResourceBundle bundle = ResourceBundle.getBundle("application");
+		String confString = bundle.getString("dao.impl");
+		Integer daoImplConf = Integer.valueOf(confString);
+
+		switch (daoImplConf) {
+		case 0:
+			lancerApplication(new PizzaDaoImplMemory());
+			break;
+		case 1:
+			lancerApplication(new PizzaDaoImplFile());
+			break;
+		default:
+			System.err.println("Aucune configuration DAO trouvé");
+			break;
+		}
+
+	}
+
+	private static void lancerApplication(IPizzaDao pizzaDao) {
 		Scanner sc = new Scanner(System.in);
-		IPizzaDao pizzaDao = new PizzaDaoImplMemory();
 		Menu menu = new Menu(sc, pizzaDao);
 		menu.afficher();
-
 	}
 
 }
